5abe62953145fa5423d3420e57365ae0abf4b387
Ammarlio <ammarhalbouni@gmail.com>
2018-05-09 02:05:58 +0300

    Signup-Standard-Done

--- Greenify-FrontEnd/App.js
+++ Greenify-FrontEnd/App.js
@@ -14,67 +14,68 @@ export default class App extends React.Component {
 
   render() {
     return (
-          <View style={styles.container}>
-          <Text style={styles.text}>Registration</Text>
-            <Image source={{uri: 'https://www.bbb.org/ProfileImages/0fc6a242-18d1-4868-8e91-abaf39dac8ac.png'}} style={{width: 400, height: 125}} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Registration</Text>
 
+        <Image source={{uri: 'https://www.bbb.org/ProfileImages/0fc6a242-18d1-4868-8e91-abaf39dac8ac.png'}} style={{width: 400, height: 125}} />
+
+        <TextInput
+      ref= {(el) => { this.username = el; }}
+      onChangeText={(username) => this.setState({username})}
+      value={this.state.username} underlineColorAndroid = "transparent"
+                 placeholder = "username"
+                 placeholderTextColor = "#9a73ef"
+                 autoCapitalize = "none"
+                 style={styles.input}
+      />
       <TextInput
-    ref= {(el) => { this.username = el; }}
-    onChangeText={(username) => this.setState({username})}
-    value={this.state.username} underlineColorAndroid = "transparent"
-               placeholder = "username"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               style={styles.input}
-    />
-    <TextInput
-    ref= {(el) => { this.password = el; }}
-    onChangeText={(password) => this.setState({password})}
-    value={this.state.password} underlineColorAndroid = "transparent"
-               placeholder = "Password"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               style={styles.input}
-    />
-    <TextInput
-    ref= {(el) => { this.phonenumber = el; }}
-    onChangeText={(phonenumber) => this.setState({phonenumber})}
-    value={this.state.phonenumber} underlineColorAndroid = "transparent"
-               placeholder = "phonenumber"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               style={styles.input}
-    />
-         <TouchableOpacity onPress={this.login} style={styles.button}>
-            <Text>Sign up</Text>
-         </TouchableOpacity>
-      </View>
+      ref= {(el) => { this.password = el; }}
+      onChangeText={(password) => this.setState({password})}
+      value={this.state.password} underlineColorAndroid = "transparent"
+                 placeholder = "Password"
+                 placeholderTextColor = "#9a73ef"
+                 autoCapitalize = "none"
+                 style={styles.input}
+      />
+      <TextInput
+      ref= {(el) => { this.phonenumber = el; }}
+      onChangeText={(phonenumber) => this.setState({phonenumber})}
+      value={this.state.phonenumber} underlineColorAndroid = "transparent"
+                 placeholder = "phonenumber"
+                 placeholderTextColor = "#9a73ef"
+                 autoCapitalize = "none"
+                 style={styles.input}
+      />
+           <TouchableOpacity onPress={this.login} style={styles.button}>
+              <Text>Sign up</Text>
+           </TouchableOpacity>
+        </View>
     );
   }
 
   login = () => {
     if(this.state.username.length && this.state.password.length && this.state.phonenumber.length  !==0){
-    fetch("http://192.168.1.76:3000/users",{
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: this.state.username,
-            password: this.state.password,
-            phonenumber:this.state.phonenumber
-        })
-    })
+      fetch("http://192.168.1.76:3000/users",{
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              username: this.state.username,
+              password: this.state.password,
+              phonenumber:this.state.phonenumber
+          })
+      })
 
-      
-        .then((responseData) => {
-           alert('Signedup Successfully')
-        })
-  }else{
-    alert('please fill all the information')
-  }
-}
+        
+          .then((responseData) => {
+             alert('Signedup Successfully')
+          })
+    }else{
+        alert('please fill all the information')
+    }
+ }
 
 }
 const styles = StyleSheet.create({
@@ -85,10 +86,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
    input: {
-      margin: 15,
-      height: 40,
-      borderColor: '#7a42f4',
-      borderWidth: 1
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
    },
    text:{
     fontSize:50,
