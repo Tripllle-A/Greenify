40650fe86cbfbca2e3014f48e9430717bbe1a90c
Ammarlio <ammarhalbouni@gmail.com>
2018-05-15 10:40:19 +0300

    eslintOnBackEnd

---
+++ Greenify-BackEnd/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+      "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+
+};
