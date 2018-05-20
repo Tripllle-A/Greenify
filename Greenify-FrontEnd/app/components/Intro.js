d5180cd078f2f107e10ac1212f05a8db7957dcc5
Ammarlio <ammarhalbouni@gmail.com>
2018-05-20 21:02:40 +0300

    eslint

--- Greenify-FrontEnd/app/components/Intro.js
+++ Greenify-FrontEnd/app/components/Intro.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Infoslider from 'react-native-infoslider';
-import { ScrollView,StyleSheet, Text, View ,TouchableOpacity,TextInput,Image} from 'react-native';
+import React from "react";
+import Infoslider from "react-native-infoslider";
+import { ScrollView,StyleSheet, Text, View ,TouchableOpacity,TextInput,Image} from "react-native";
 
 export default class Intro extends React.Component {
     constructor(){
@@ -11,19 +11,19 @@ export default class Intro extends React.Component {
           {
           title:"Hello World", 
           text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard",
-          image: {uri:'https://cdn-blog.credihealth.com/wp-content/uploads/2017/11/Indoor-Air-Purifying-Plants-India-660x330.jpg'}},
+          image: {uri:"https://cdn-blog.credihealth.com/wp-content/uploads/2017/11/Indoor-Air-Purifying-Plants-India-660x330.jpg"}},
           {
           title:"Lorem Ipsum", 
           text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard",
-          image: {uri:'https://opimedia.azureedge.net/-/media/images/men/editorial/articles/magazine-articles/2018/04-01/top-plants-for-companion-planting/companion-1-jpg.jpg'}},
+          image: {uri:"https://opimedia.azureedge.net/-/media/images/men/editorial/articles/magazine-articles/2018/04-01/top-plants-for-companion-planting/companion-1-jpg.jpg"}},
           {
           title:"Lorem Ipsum", 
           text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard",
-          image: {uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS25qBQfw-dudK4A1B_VuOymWhbTabSbVNTJp1yvJFDFE393qBMIQ'}},
+          image: {uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS25qBQfw-dudK4A1B_VuOymWhbTabSbVNTJp1yvJFDFE393qBMIQ"}},
           {
           title:"Lorem Ipsum", 
           text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard",
-          image: {uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOTGJex1uCNQGNdAgaL1SGQx4XXHhFo2FXdo3midbjGB7W1t8O'}},
+          image: {uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOTGJex1uCNQGNdAgaL1SGQx4XXHhFo2FXdo3midbjGB7W1t8O"}},
           
       ]
 
@@ -49,17 +49,17 @@ export default class Intro extends React.Component {
         autoplayTimeout={3}/>
         </View>
         <View style={styles.container}>
-        <Image source={{uri:'https://www.maximumventure.ca/wp-content/uploads/2017/04/greenify-logo.jpg'}} style={{width: 400, height: 125}}/>
+        <Image source={{uri:"https://www.maximumventure.ca/wp-content/uploads/2017/04/greenify-logo.jpg"}} style={{width: 400, height: 125}}/>
         </View>
       
-        <View style={{ justifyContent: 'center', alignItems: 'center', flexDirection: 'row', flex: 1}}>
+        <View style={{ justifyContent: "center", alignItems: "center", flexDirection: "row", flex: 1}}>
   <View style={styles.margin}>
-     <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')} style={styles.button}>
+     <TouchableOpacity onPress={() => this.props.navigation.navigate("Login")} style={styles.button}>
               <Text>Sign In</Text>
            </TouchableOpacity>
   </View>
   <View>
-    <TouchableOpacity onPress={() => this.props.navigation.navigate('Signup')} style={styles.button}>
+    <TouchableOpacity onPress={() => this.props.navigation.navigate("Signup")} style={styles.button}>
               <Text>Sign Up</Text>
            </TouchableOpacity>
   </View>
@@ -79,14 +79,14 @@ export default class Intro extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
    
-    justifyContent: 'center',
+    justifyContent: "center",
   },
    input: {
     margin: 15,
     height: 40,
-    borderColor: '#7a42f4',
+    borderColor: "#7a42f4",
     borderWidth: 1
    },
    margin: {
@@ -95,13 +95,13 @@ const styles = StyleSheet.create({
    },
    text:{
     fontSize:50,
-    textAlign:'center',
+    textAlign:"center",
     paddingBottom:60,
    },
   button: {
     padding:20,
     borderWidth:1,
-    backgroundColor:'green'
+    backgroundColor:"green"
   },
   contentContainer:{
     paddingVertical: 20
--- Greenify-FrontEnd/app/components/Profile.js
+++ Greenify-FrontEnd/app/components/Profile.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import { StyleSheet, Text, View ,TouchableOpacity,Image,ImageBackground} from 'react-native';
+import { StyleSheet, Text, View ,TouchableOpacity,ImageBackground} from "react-native";
 
 export default class ProfileIntro extends React.Component {
     constructor(){
@@ -13,34 +13,34 @@ export default class ProfileIntro extends React.Component {
       <View style={styles.container}>
       <View style={styles.container}>
       <TouchableOpacity style={styles.container} 
-      onPress={() => this.props.navigation.navigate('MyPlants')}>
+      onPress={() => this.props.navigation.navigate("MyPlants")}>
              <ImageBackground
         style={{
-          backgroundColor: '#ccc',
+          backgroundColor: "#ccc",
           flex: 1,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          justifyContent: 'center',
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
         }}
-        source={{ uri: 'http://www.limerickgardentrail.com/wp-content/uploads/plants-for-home-dont-have-much-space-to-grow-your-plants-at-home-meshpedia.jpg' }}
+        source={{ uri: "http://www.limerickgardentrail.com/wp-content/uploads/plants-for-home-dont-have-much-space-to-grow-your-plants-at-home-meshpedia.jpg" }}
       >
       </ImageBackground>
       </TouchableOpacity>
       </View>
       <View style={styles.container}>
       <TouchableOpacity style={styles.container}
-      onPress={() => this.props.navigation.navigate('PlantsDisplay')} >
+      onPress={() => this.props.navigation.navigate("PlantsDisplay")} >
              <ImageBackground
         style={{
-          backgroundColor: '#ccc',
+          backgroundColor: "#ccc",
           flex: 1,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          justifyContent: 'center',
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
         }}
-        source={{ uri: 'https://simplytreesfl.com/wp-content/uploads/2018/01/simply-trees-llc-hand-photo-webop.png' }}
+        source={{ uri: "https://simplytreesfl.com/wp-content/uploads/2018/01/simply-trees-llc-hand-photo-webop.png" }}
       >
       </ImageBackground>
       </TouchableOpacity>
@@ -53,35 +53,35 @@ export default class ProfileIntro extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
    container1: {
     flex: 1,
-    backgroundColor: '#d3ffce',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d3ffce",
+    alignItems: "center",
+    justifyContent: "center",
   },
    container2: {
     flex: 1,
-    backgroundColor: '#b3ffe1',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#b3ffe1",
+    alignItems: "center",
+    justifyContent: "center",
   },
    input: {
     margin: 15,
     height: 40,
-    borderColor: '#7a42f4',
+    borderColor: "#7a42f4",
     borderWidth: 1
    },
    text:{
     fontSize:50,
-    textAlign:'center',
+    textAlign:"center",
     paddingBottom:60,
    },
   button: {
     padding:20,
     marginBottom:10,
     borderWidth:1,
-    backgroundColor:'#b3ffe1'
+    backgroundColor:"#b3ffe1"
   }
 });
